251aee135badb04e8466ba8522e16b813795f45c
RealMG <realmg51@gmail.com>
2026-03-07 09:58:54 +0700

    Create Logo.cs

---
+++ Compiler/Main/UI/Logo.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Fminusminus.Main.UI
+{
+    /// <summary>
+    /// Display F-- logo
+    /// </summary>
+    public static class Logo
+    {
+        public static void Display()
+        {
+            Console.WriteLine(@"
+    ╔══════════════════════════════════════╗
+    ║  ███████╗  ███╗   ███╗  ██╗██╗      ║
+    ║  ██╔════╝  ████╗ ████║  ██║██║      ║
+    ║  █████╗    ██╔████╔██║  ██║██║      ║
+    ║  ██╔══╝    ██║╚██╔╝██║  ██║██║      ║
+    ║  ██║       ██║ ╚═╝ ██║  ██║██║      ║
+    ║  ╚═╝       ╚═╝     ╚═╝  ╚═╝╚═╝      ║
+    ║                                      ║
+    ║     F-- PROGRAMMING LANGUAGE         ║
+    ║        Version 2.0.0.0-alpha1        ║
+    ║     Created by RealMG (13 tuổi)      ║
+    ║    Contributors: chaunguyen12477     ║
+    ╚══════════════════════════════════════╝
+            ");
+        }
+    }
+}
