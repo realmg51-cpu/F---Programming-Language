4aac1cd40c1ce5cc46d8b80ccacd7098d1a32bc2
RealMG <realmg51@gmail.com>
2026-03-07 08:18:45 +0700

    Update IOListNode.cs

--- Compiler/Nodes/IOListNode.cs
+++ Compiler/Nodes/IOListNode.cs
@@ -1,13 +1,8 @@
-using System.Collections.Generic;
-
 namespace Fminusminus
 {
-    /// <summary>
-    /// IO list file node
-    /// </summary>
     public class IOListNode : StatementNode
     {
-        public ExpressionNode Path { get; set; }
+        public ExpressionNode? Path { get; set; }
         public bool UseOSPath { get; set; }
         
         public override void Print(int indent = 0)
